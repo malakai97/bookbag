cd420a18e3cc8b562b034deb0d40e6cab007cf50
Bryan Hockey <bhock@umich.edu>
2015-09-02 16:52:21 -0400

    revert attempt to gemify

--- lib/bookbag.rb
+++ lib/bookbag.rb
@@ -1,4 +1,4 @@
-require "bookbag/version"
+require "bundler/setup"
 require "bookbag/settings"
 require "bookbag/builder"
 require "bookbag/dpn_info_txt"
--- lib/bookbag/version.rb
+++
@@ -1,3 +0,0 @@
-module Bookbag
-  VERSION = "0.1.0"
-end
